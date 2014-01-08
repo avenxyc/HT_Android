f21b24fe5db57a59e1b329dfebf3bd9a170aaeb7
Yichuan Xu <avenxyc@gmail.com>
2014-01-07 20:22:18 -0400

    Launch Barcode Scanner
    #TODO: how to save the value and display it

--- src/com/aven/ht_android/MainActivity.java
+++ src/com/aven/ht_android/MainActivity.java
@@ -1,15 +1,20 @@
 package com.aven.ht_android;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
+	 public final static String result = "result";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
 	}
 
 	@Override
@@ -19,4 +24,26 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	public void launchCamera (View view){
+		 Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+	     intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+	     startActivityForResult(intent, 0);
+	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+	    if (requestCode == 0) {
+	        if (resultCode == RESULT_OK) {
+	            String contents = intent.getStringExtra("SCAN_RESULT");
+	            String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+
+	            Intent passValue = new Intent(this, CameraActivity.class);
+	            passValue.putExtra(result, contents);
+	            startActivity(passValue);
+	            
+	        } else if (resultCode == RESULT_CANCELED) {
+	            // Handle cancel
+	        }
+	    }
+	}
 }
