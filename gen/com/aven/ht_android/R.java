3704c7afdb51014ab64f7cfcdea63ffab5b2cf43
Yichuan Xu <avenxyc@gmail.com>
2014-01-15 14:23:26 -0400

    Small fix

--- gen/com/aven/ht_android/R.java
+++ gen/com/aven/ht_android/R.java
@@ -43,8 +43,8 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int camera=0x7f050003;
         public static final int databasetest=0x7f050005;
+        public static final int defaultText=0x7f050002;
         public static final int get_item_info=0x7f050006;
-        public static final int hello_world=0x7f050002;
         public static final int title_activity_camera=0x7f050004;
     }
     public static final class style {
